de92f048d7a730b99785495c4f3f140b4f550267
Victoria Castro <victoria.akicastro92@gmail.com>
2017-11-01 20:57:43 -0600

    estructura presentada para revision de parcial

---
+++ js/logScript.js
@@ -0,0 +1,8 @@
+document.getElementById("registro").style.display = "block";
+
+function mostrarRegistro(){
+		document.getElementById("registro").style.display = "block";
+		document.getElementById("login").style.display = "none";
+
+		//document.getElementById("registrarse").innerHTML="Login";
+}
